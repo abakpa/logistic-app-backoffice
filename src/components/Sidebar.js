b6e8384844cefcf560635d3d4117c4a18c288558
Joseph Abakpa <elachi4c@yahoo.com>
2024-11-07 11:46:12 +0100

    toggle sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -30,85 +30,85 @@ const Sidebar = () => {
       <nav className="flex-1 px-4 py-6 overflow-y-auto">
         <ul className="space-y-4 text-sm">
           <li>
-            <Link to="/home" className="flex items-center space-x-2 mt-14 md:mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/home" onClick={toggleSidebar} className="flex items-center space-x-2 mt-14 md:mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaHome className="text-sm" />
               <span>Home</span>
             </Link>
           </li>
           <li>
-            <Link to="/admin" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/admin" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaUsers className="text-sm" />
               <span>Staff</span>
             </Link>
           </li>
           <li>
-            <Link to="/createstaff" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/createstaff" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Create Staff</span>
             </Link>
           </li>
           <li>
-            <Link to="/users" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/users" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaUsers className="text-sm" />
               <span>Users</span>
             </Link>
           </li>
           <li>
-            <Link to="/newuser" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/newuser" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Create User</span>
             </Link>
           </li>
           <li>
-            <Link to="/vehicles" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/vehicles" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaTruck className="text-sm" />
               <span>Vehicles</span>
             </Link>
           </li>
           <li>
-            <Link to="/newvehicle" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/newvehicle" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Create Vehicle</span>
             </Link>
           </li>
           <li>
-            <Link to="/drivers" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/drivers" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaTruckLoading className="text-sm" />
               <span>Drivers</span>
             </Link>
           </li>
           <li>
-            <Link to="/newdrivers" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/newdrivers" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Create Driver</span>
             </Link>
           </li>
           <li>
-            <Link to="/orderbydriver" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/orderbydriver" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaClipboardList className="text-sm" />
               <span>Driver's Order</span>
             </Link>
           </li>
           <li>
-            <Link to="/order" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/order" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaClipboardList className="text-sm" />
               <span>Orders</span>
             </Link>
           </li>
           <li>
-            <Link to="/neworder" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/neworder" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Create Order</span>
             </Link>
           </li>
           <li>
-            <Link to="/orderforpickup" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/orderforpickup" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaPlus className="text-sm" />
               <span>Order for Pick Up</span>
             </Link>
           </li>
           <li>
-            <Link to="/dispatch" className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
+            <Link to="/dispatch" onClick={toggleSidebar} className="flex items-center space-x-2 mt-6 px-4 rounded-lg hover:bg-gray-200">
               <FaClipboardList className="text-sm" />
               <span>Dispatch</span>
             </Link>
